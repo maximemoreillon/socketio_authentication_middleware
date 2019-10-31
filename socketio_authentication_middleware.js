104326b20924edd956d4769143f493881851f1fa
Maxime Moreillon <m.moreillon@gmail.com>
2019-10-31 04:33:39 +0000

    working prototype

--- socketio_authentication_middleware.js
+++ socketio_authentication_middleware.js
@@ -8,57 +8,72 @@ exports.authentication_middleware = function (socket, token_verification, creden
   // return the actual middleware
   return function(packet, next){
 
-    // If client previosuly authorized or posesses a valid JWT
+    // Parsing packet
+    var ws_event = packet[0];
+    var ws_payload = packet[1];
+
+    // If client was previosuly authorized
     if(socket.authenticated) {
-      next();
+
+      // Unless the client is logging out, allow him to continue
+      if(ws_event === 'logout') {
+        socket.authenticated = false;
+        socket.leave('authenticated');
+        socket.emit('unauthorized','logged out');
+      }
+      else {
+        next();
+      }
     }
     else {
       // If the client is not authenticated, only allow him to login
-
-      // Parsing packet
-      var ws_event = packet[0];
-      var ws_payload = packet[1];
-
-      if(ws_event === 'authenticate'){
+      if(ws_event === 'token_authentication'){
         // The user is trying to authenticate using a JWT
 
         // TODO: VERIFY TOKEN PROPERLY
-        if(token_verification(ws_payload.token)){
-          // The token is valid
+        token_verification(ws_payload.jwt, function(err, res){
+          if(res){
+            // The token is valid
 
-          // For future connections, no need to check anymore
-          socket.authenticated = true;
+            // For future connections, no need to check anymore
+            socket.authenticated = true;
 
-          // NOT SURE ABOUT THIS
-          socket.emit('authenticated',{});
-        }
-        else {
-          // JWT is invalid
-          socket.emit('unauthorized','Invalid JWT');
-        }
+            // Joining room
+            socket.join('authenticated');
+
+            // Emit confirmation message
+            socket.emit('authenticated',res);
+          }
+          else {
+            // JWT is invalid
+            socket.emit('unauthorized','Invalid JWT');
+          }
+        })
+
       }
 
-      else if(ws_event === 'login'){
+      else if(ws_event === 'credentials_authentication'){
         // The client is trying to login with a username/password combo
 
-        // TODO: externalize this function
-        if(credentials_verification(ws_payload.credentials)){
-          // The credentials are correct
+        credentials_verification(ws_payload.credentials, function(err,res){
+          if(res){
+            // The credentials are correct
 
-          // Emit a token so the user does not need to login for next connections
-          // TODO: GENERATE PROPER TOKEN
-          socket.emit('token','theToken');
+            // Allow access for future packets
+            socket.authenticated = true;
 
-          // NOT SURE ABOUT THIS
-          socket.emit('authenticated', {});
+            // joining room
+            socket.join('authenticated');
 
-          // Allow access for future packets
-          socket.authenticated = true;
-        }
-        else {
-          // Login Credentials are wrong
-          socket.emit('unauthorized','Invalid credentials');
-        }
+            // Emit a token so the user does not need to login for next connections
+            socket.emit('authenticated', res);
+          }
+          else {
+            // Login Credentials are wrong
+            socket.emit('unauthorized','Invalid credentials');
+          }
+        })
+
       }
       else {
         // The user is trying to do something else than authenticating
