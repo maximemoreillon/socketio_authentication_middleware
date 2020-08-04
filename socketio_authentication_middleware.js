e6e686e9b87e393078ea0a8918d0dbbfedd638e1
Maxime Moreillon <m.moreillon@gmail.com>
2020-08-04 12:56:03 +0000

    slightly more modern syntax

--- socketio_authentication_middleware.js
+++ socketio_authentication_middleware.js
@@ -3,57 +3,52 @@ The middleware returns the function that is used by socket.use
 This is so as to acces the socket object
 */
 
-module.exports = function (socket, authentication_function){
+module.exports = (socket, authentication_function) => {
 
   // return the actual middleware
-  return function(packet, next){
+  return (packet, next) => {
 
     // Parsing packet
-    var ws_event = packet[0];
-    var ws_payload = packet[1];
+    let ws_event = packet[0]
+    let ws_payload = packet[1]
 
     // If client was previosuly authorized
     if(socket.authenticated) {
 
       // Unless the client is logging out, allow him to continue
       if(ws_event === 'logout') {
-        socket.authenticated = false;
-        socket.leave('authenticated');
-        socket.emit('unauthorized','logged out');
+        socket.authenticated = false
+        socket.leave('authenticated')
+        socket.emit('unauthorized','logged out')
       }
       else {
-        next();
+        next()
       }
+
     }
     else {
-      // If the client is not authenticated, only allow him to login
-      if(ws_event === 'authentication'){
-        // The user is trying to authenticate
-
-        authentication_function(ws_payload, (err, res) => {
-          if(res){
-            // The token is valid
-
-            // For future connections, no need to check anymore
-            socket.authenticated = true;
-
-            // Joining room
-            socket.join('authenticated');
-
-            // Emit confirmation message
-            socket.emit('authenticated',res);
-          }
-          else {
-            // JWT is invalid
-            socket.emit('unauthorized','Invalid JWT');
-          }
-        })
-
+      // If The user is trying to do something else than authenticating
+      if(ws_event !== 'authentication') {
+        return socket.emit('unauthorized','Authentication required')
       }
-      else {
-        // The user is trying to do something else than authenticating
-        socket.emit('unauthorized','Authentication required');
-      }
+
+      // If The user is trying to authenticate, forward credentials to authentication callback
+      // IDEA: Could pass the socket here
+      authentication_function(ws_payload, (error, response) => {
+        if(err) return socket.emit('unauthorized', error)
+          // The user authenticated properly
+
+        // For future connections, no need to check anymore
+        socket.authenticated = true
+
+        // Joining room of authenticated users
+        socket.join('authenticated')
+
+        // Emit confirmation message
+        socket.emit('authenticated', response)
+
+      })
+
     }
   }
 
